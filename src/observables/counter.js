4955741584f8726a69128191c2d3eaa8ea73a871
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-04-28 09:15:24 +0200

    fix: add set on counter store

--- src/observables/counter.js
+++ src/observables/counter.js
@@ -1,13 +1,17 @@
 import { Subject } from 'rxjs';
 
 const store = {
-  counter: 0
-}
+  counter: 0,
+
+  set updateCounter(counter) {
+    this.counter = counter;
+  }
+};
 
 export const counterSubject = new Subject();
 
 export const updateCounter = (value) => {
-  store.counter = (store.counter + value);
+  store.updateCounter = (store.counter + value);
 
   counterSubject.next(store.counter);
 };
